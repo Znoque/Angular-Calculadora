8bf3b5480557ac6aefdc4efe792c370d63779f3f
Enoque Teixeira <znoque5@gmail.com>
2021-01-19 15:13:48 -0300

    Implementação do teste do serviço- calculadora

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
@@ -13,4 +13,45 @@ describe('CalculadoraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // Testa o calculo de soma da calculadora
+  it('deve garantir que 2 + 2 = 4',
+   inject([CalculadoraService], (service: CalculadoraService) => {
+     let soma = service.calcular(2, 2, CalculadoraService.SOMA);
+     expect(soma).toEqual(4);
+   })
+  );
+
+  // Testa o calculo de subtração da calculadora
+  it('deve garantir que 10 - 5 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let sub = service.calcular(10, 5, CalculadoraService.SUB);
+      expect(sub).toEqual(5);
+    })
+  );
+  
+  // Testa o calculo de divisão da calculadora
+  it('deve garantir que 8 / 4 = 2',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let div = service.calcular(8, 4, CalculadoraService.DIV);
+      expect(div).toEqual(2);
+    })
+  );
+
+  // Testa o calculo de multiplicação da calculadora
+  it('deve garantir que 4 X 4 = 16',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let multi = service.calcular(4, 4, CalculadoraService.MUL);
+      expect(multi).toEqual(16);
+    })
+  );
+
+  // Testa uma operação inválida da calculadora
+  it('deve retornar 0 para operação inválida',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let opInvalida = service.calcular(1, 2, '%');
+      expect(opInvalida).toEqual(0);
+    })
+  );
+
 });
