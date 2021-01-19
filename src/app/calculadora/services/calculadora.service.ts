19dfa276010d3e9ce6bf8430ecd985d3f53ee6ad
Enoque Teixeira <znoque5@gmail.com>
2021-01-19 14:12:55 -0300

    Implementação & Documentação do Serviço Calculadora

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,3 +1,11 @@
+/** 
+ * Serviço responsável por executar as operações
+ * da calculadora.
+ * 
+ * @author Enoque T. Barbosa<znoque5@gmail.com>
+ * @since 1.0.0
+ */ 
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +13,45 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  // Define as constantes utilizadas para as operações de cálculo.
+  static readonly SOMA: string = '+';
+  static readonly SUB: string = '-';
+  static readonly DIV: string = '/';
+  static readonly MUL: string = '*';
+
   constructor() { }
+
+  /** 
+   * Método que Calcula uma operação matemática dado
+   * dois números.
+   * Suporta as operações soma, subtração, divisão,
+   * e multiplicação.
+   * 
+   * @param n1 number Armazena o primeiro número
+   * @param n2 number Armazena o segundo número
+   * @param op string Operação a ser executada
+   * @return number Resultado da operação
+   */
+  calcular(n1: number, n2: number, op: string): number{
+    let r: number = 0; // Armazena o resultado da operação.
+
+    switch(op){
+      case CalculadoraService.SOMA:
+        r = n1 + n2;
+      break;
+      case CalculadoraService.SUB:
+        r = n1 - n2;
+      break;
+      case CalculadoraService.DIV:
+        r = n1 / n2;
+      break;
+      case CalculadoraService.MUL:
+        r = n1 * n2;
+      break;
+      default:
+        r = 0;
+    }
+
+    return r;
+  }
 }
